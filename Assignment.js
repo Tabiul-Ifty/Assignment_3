669006bd1e687d0f8b3c167cbb62a4b54cd5c9e1
tabiulifty <tabiulifty@gmail.com>
2021-01-22 19:44:34 +0600

    Done all

--- Assignment.js
+++ Assignment.js
@@ -48,5 +48,59 @@ function budgetCalculator(number_of_watch,number_of_tv,number_of_laptop)
 // budgetCalculator(10,20,3);                                            
 }
 
+function hotelCost(number_of_day)
+{
+    var total_bill=0;
+    if(typeof number_of_day !='number')
+    {
+        console.log("Please input some numbes");   //input not a number.
+        return ;
+    }
+    else if(number_of_day<0)
+    {
+        console.log("Days can't be negative. Please input some real values.");    //input as a negative number.
+        return ;
+    }
+    else if(number_of_day>=0 && number_of_day<=10)           //first 10 days    
+    {
+         total_bill= number_of_day * 100;
+         return total_bill;
+    }
+    else if(number_of_day>10 && number_of_day<=20)       //upto 20 days
+    {
+        total_bill= (number_of_day-10)*80 + 10*100;
+        return total_bill;
+    }
+    else if(number_of_day>21)                       //more than 20 days
+    {
+        total_bill= 10*(100+80)+ (number_of_day-20)*50;
+        return total_bill;
+    }
+// test case:->
+// hotelCost(-1);
+// hotelCost(5);
+// hotelCost(15);
+// hotelCost(25);
+}
+
+function megaFriend(string_array)
+{
+    var size_of_name=0;
+    var max_length_name="";
+    for(var i=0; i<string_array.length; i++)
+    {
+        if(string_array[i].length> size_of_name)
+        {
+            size_of_name=string_array[i].length;
+            max_length_name=string_array[i];
+        }
+    }
+    return max_length_name;  //returning the maximum lenght's name
+}
+
+var name_of_friends=["Goku","Berussama","Buu","Kilua","25th Bam","Endorsi","Anak Jahad", "Pentaminum"];
+
+console.log(megaFriend(name_of_friends));
 console.log(kilometerToMeter(2));
-console.log(budgetCalculator(5,2,1))
+console.log(budgetCalculator(5,2,1));
+console.log(hotelCost(23));
